c29fb23b5bfb32da6ce59bf61e7bb9563186116c
Vijay <t.vijaykumar7356@gmail.com>
2024-10-26 17:51:20 +0530

    Updates done in the code

--- src/test/java/base/baseTest.java
+++ src/test/java/base/baseTest.java
@@ -3,56 +3,25 @@ package base;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
-
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.testng.annotations.BeforeClass;
 
 public class baseTest {
 
-	public static WebDriver driver;
-	public static Properties pro = new Properties();
-	public static Properties loc = new Properties();
-	public static FileReader fr;
-	public static FileReader fr1;
-	@BeforeMethod
-	public void Setup() throws IOException {
-		if(driver==null) {
-			
-			 fr = new FileReader(System.getProperty("user.dir")+"\\src\\test\\resources\\configfiles\\cofig.properties");
-			 fr1 = new FileReader(System.getProperty("user.dir") +"\\src\\test\\resources\\configfiles\\locators.properties");
-			pro.load(fr);
-			loc.load(fr1);
-		}
-		if(pro.getProperty("browser").equalsIgnoreCase("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
-			driver.get(pro.getProperty("testurl"));
-			
-		}
-		if(pro.getProperty("browser").equalsIgnoreCase("edge")) {
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
-			driver.get(pro.getProperty("testurl"));
-		}
-		if(pro.getProperty("browser").equalsIgnoreCase("Firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-			driver.get(pro.getProperty("testurl"));
-		}
-		
-	}
-	@AfterMethod()
-	public void tearDown() {
-		driver.close();
-		System.out.println("Teardown succesful");
-	}
-	
+    protected JSONArray testData;
 
-
+    @BeforeClass
+    public void loadTestData() {
+        try {
+            FileReader reader = new FileReader("C:\\eclipse-workspace\\Caw_Studio_Assignment\\src\\test\\resources\\testdata\\data.json");
+            JSONParser parser = new JSONParser();
+            this.testData = (JSONArray) parser.parse(reader);
+        } catch (Exception e) {
+            System.err.println("Error loading test data: " + e.getMessage());
+            
+        }
+        
+    }
 }
---
+++ src/test/java/testcase/DynamicTableTest.java
@@ -0,0 +1,85 @@
+package testcase;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import base.baseTest;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class DynamicTableTest extends baseTest {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        WebDriverManager.edgedriver().setup();
+        driver = new EdgeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+   
+    @Test
+    public void testDynamicTable() {
+        if (this.testData == null) {
+            System.err.println("Test data is not loaded. Skipping test.");
+            return;
+        }
+        
+        driver.get("https://testpages.herokuapp.com/styled/tag/dynamic-table.html");
+
+        // Step 1: Click "Table Data" button
+        WebElement tableDataButton = driver.findElement(By.xpath("/html/body/div/div[3]/details/summary"));
+        tableDataButton.click();
+
+        // Step 2: Locate the input box and enter JSON data as a single string
+        WebElement inputBox = driver.findElement(By.id("jsondata"));
+        inputBox.clear();
+        inputBox.sendKeys(testData.toJSONString());
+
+        // Step 3: Click "Refresh Table" to populate the table with JSON data
+        WebElement refreshTableButton = driver.findElement(By.id("refreshtable"));
+        refreshTableButton.click();
+
+        // Step 4: Validate the populated data
+        List<WebElement> rows = driver.findElements(By.xpath("//tr"));
+        
+        // Adjusting row count to match the number of data entries
+        Assert.assertEquals(rows.size(), testData.size() + 1, "Row count mismatch!");  // +1 for the header
+
+        for (int i = 1; i < rows.size(); i++) { // Start from 1 to skip header
+            JSONObject expectedData = (JSONObject) testData.get(i - 1); // Get expected data
+            WebElement row = rows.get(i);
+            List<WebElement> columns = row.findElements(By.tagName("td"));
+
+            // Logging actual values for debugging
+            System.out.println("Row " + i + " values: ");
+            for (int j = 0; j < columns.size(); j++) {
+                System.out.println("Column " + j + ": " + columns.get(j).getText());
+            }
+
+            // Validate each cell against corresponding JSON data (updated order)
+            Assert.assertEquals(columns.get(0).getText(), expectedData.get("gender").toString(), "Gender mismatch!");  // Gender in column 0
+            Assert.assertEquals(columns.get(1).getText(), expectedData.get("name").toString(), "Name mismatch!");  // Name in column 1
+            Assert.assertEquals(columns.get(2).getText(), expectedData.get("age").toString(), "Age mismatch!");    // Age in column 2
+        }
+    }
+
+
+    @AfterClass
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
