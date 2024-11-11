8df2e48ab6012fbbe144e62fe64458af2922776d
Vijay <t.vijaykumar7356@gmail.com>
2024-11-11 17:25:44 +0530

    New code changes

--- Jenkins.bat
+++ Jenkins.bat
@@ -10,11 +10,9 @@ pipeline {
         stage('Build') {
             steps {
                 // Get some code from a GitHub repository
-                git 'https://github.com/vijaykumart2523/Caw_Studio_assginments.git/'
+                git 'https://github.com/vijaykumart2523/Caw_Studio_assginments.git'
 
-                // Run Maven on a Unix agent.
-              //  sh "mvn -Dmaven.test.failure.ignore=true clean package"
-
+               
                 // To run Maven on a Windows agent, use
                  bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
