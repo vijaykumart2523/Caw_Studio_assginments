b8cf5de9318372459496914a8839f54eea90496f
Vijay <t.vijaykumar7356@gmail.com>
2024-11-11 17:34:11 +0530

    New code changes

--- Jenkins.bat
+++ Jenkins.bat
@@ -1,30 +1,37 @@
 pipeline {
     agent any
 
-    tools {
-        // Install the Maven version configured as "M3" and add it to the path.
-        maven "maven"
-    }
-
     stages {
+        stage('Checkout') {
+            steps {
+                // Use the credentials ID to access the repository
+                git credentialsId: '90fe5ba5-a758-4bfb-b85b-7f101a13e0b9', url: 'https://github.com/vijaykumart2523/Caw_Studio_assginments.git', branch: 'main'
+            }
+        }
         stage('Build') {
             steps {
-                // Get some code from a GitHub repository
-                git 'https://github.com/vijaykumart2523/Caw_Studio_assginments.git'
-
-               
-                // To run Maven on a Windows agent, use
-                 bat "mvn -Dmaven.test.failure.ignore=true clean package"
+                // Build the project using Maven
+                sh 'mvn clean compile'
             }
-
-            post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
-                success {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
-                }
+        }
+        stage('Test') {
+            steps {
+                // Run Selenium tests
+                sh 'mvn test'
             }
+        }
+        stage('Deploy') {
+            steps {
+                // Deploy the application (if applicable)
+                sh 'mvn deploy'
+            }
+        }
+    }
+
+    post {
+        always {
+            // Actions that should be executed at the end of the pipeline
+            echo 'Pipeline finished.'
         }
     }
 }
