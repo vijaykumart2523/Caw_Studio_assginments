e4a7ef7bb2e9090365cd24ec550d236897c974b4
vijaykumart2523 <120502872+vijaykumart2523@users.noreply.github.com>
2024-11-19 16:44:09 +0530

    Jenkinsfile.bat

--- Jenkinsfile.bat
+++ Jenkinsfile.bat
@@ -2,28 +2,29 @@ pipeline {
     agent any
 
     tools {
-        // Install the Maven version configured as "maven" and add it to the path.
         maven "maven"
     }
 
     stages {
+        stage('Checkout') {
+            steps {
+                git url: 'https://github.com/vijaykumart2523/Caw_Studio_assginments.git', branch: 'main'
+            }
+        }
         stage('Build') {
             steps {
-                // Get some code from a GitHub repository
-                git 'https://github.com/vijaykumart2523/Caw_Studio_assginments.git'
-
-                // To run Maven on a Windows agent, use
-                bat "mvn -Dmaven.test.failure.ignore=true clean package"
+                bat "mvn clean test"
             }
+        }
+    }
 
-            post {
-                // If Maven was able to run the tests, even if some of the tests
-                // failed, record the test results and archive the jar file.
-                success {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
-                }
-            }
+    post {
+        success {
+            junit '**/target/surefire-reports/TEST-*.xml'
+            archiveArtifacts artifacts: 'target/*.jar', allowEmptyArchive: true
+        }
+        failure {
+            echo "Build failed"
         }
     }
 }
